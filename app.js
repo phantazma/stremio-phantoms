11b6744554d655dee17321392464fdcfd5f5e94e
phantazma <nabil.amenchar1@gmail.com>
2023-06-14 10:04:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const addonInterface = require("./index");
 const router = getRouter(addonInterface);
 module.exports = function(req, res) {
     router(req, res, function() {
+        console.log(req);
         res.statusCode = 404;
         res.end();
     });
